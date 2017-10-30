7c006e9996113879de421b2b694976e317d40555
Jon Thysell <thysell@gmail.com>
2017-10-30 16:57:51 -0700

    Pong updates * Desync game from framerate * Fixed speeds, paddle bouncing * Added debug mode to view FPS * Balls now go all the way to the walls

--- pong/main.lua
+++ pong/main.lua
@@ -23,21 +23,25 @@ ball = {
     dy = 0
 }
 
-paddleSpeed = 0.2
-ballSpeed = 0.01
+paddleSpeed = 3
+ballSpeed = 0.5
 
-paddleBounce = 1.5
+paddleBounce = 1.1
 
 scoreToWin = 10
 
 started = false
 
+debugMode = false
+
 function resetBall()
     ball.x = (love.graphics.getWidth() - ballSize) / 2
     ball.y = (love.graphics.getHeight() - ballSize) / 2
     
-    ball.dx = -1 + 2 * math.random()
-    ball.dy = -1 + 2 * math.random()
+    ball.dx = 0.5 + 0.5 * math.random()
+    if math.random() > 0.5 then ball.dx = -ball.dx end
+    ball.dy = 0.5 + 0.5 * math.random()
+    if math.random() > 0.5 then ball.dy = -ball.dy end
 end
 
 function initPaddles(reset)
@@ -69,8 +73,17 @@ function love.load()
     initPaddles(true)
 end
 
-function love.update()
+function love.resize()
     initPaddles(false)
+end
+
+function love.update(dt)
+    if love.keyboard.isDown("q") then
+        love.window.close()
+    elseif love.keyboard.isDown("d") then
+        debugMode = not debugMode
+        love.timer.sleep(0.10)
+    end
     
     if not started then
         if love.keyboard.isDown("down") or love.keyboard.isDown("up") then
@@ -79,10 +92,10 @@ function love.update()
     else
         -- Process input for left
         if love.keyboard.isDown("down") then
-            left.y = left.y + (paddleHeight * paddleSpeed)
+            left.y = left.y + (paddleHeight * paddleSpeed * dt)
             left.moving = true
         elseif love.keyboard.isDown("up") then
-            left.y = left.y - (paddleHeight * paddleSpeed)
+            left.y = left.y - (paddleHeight * paddleSpeed * dt)
             left.moving = true
         else
             left.moving = false
@@ -91,10 +104,10 @@ function love.update()
         -- Process AI for right
         if ball.dx > 0 then
             if ball.y + (ballSize / 2) > right.y + paddleHeight then
-                right.y = right.y + (paddleHeight * paddleSpeed)
+                right.y = right.y + (paddleHeight * paddleSpeed * dt)
                 right.moving = true
             elseif ball.y + (ballSize / 2) < right.y then
-                right.y = right.y - (paddleHeight * paddleSpeed)
+                right.y = right.y - (paddleHeight * paddleSpeed * dt)
                 right.moving = true
             else
                 right.moving = false
@@ -111,8 +124,8 @@ function love.update()
         ball.dx = math.min(1, math.max(ball.dx, -1))
         ball.dy = math.min(1, math.max(ball.dy, -1))
         
-        ball.x = ball.x + (ball.dx * ballSpeed * math.min(love.graphics.getHeight(), love.graphics.getWidth()))
-        ball.y = ball.y + (ball.dy * ballSpeed * math.min(love.graphics.getHeight(), love.graphics.getWidth()))
+        ball.x = ball.x + (ball.dx * ballSpeed * math.min(love.graphics.getHeight(), love.graphics.getWidth()) * dt)
+        ball.y = ball.y + (ball.dy * ballSpeed * math.min(love.graphics.getHeight(), love.graphics.getWidth()) * dt)
         
         -- Process wall/ball collisions
         if ball.y < 0 then
@@ -125,36 +138,30 @@ function love.update()
         
         if ball.x < left.x + paddleWidth then
             -- ball is crossing into left
-            if ball.y > left.y - ballSize and ball.y < left.y + paddleHeight then
+            if ball.x > left.x and ball.y > left.y - ballSize and ball.y < left.y + paddleHeight then
                 -- left hits ball
                 ball.x = left.x + paddleWidth
                 ball.dx = -ball.dx
                 if left.moving then
                     ball.dx = ball.dx * paddleBounce
                     ball.dy = ball.dy * paddleBounce
-                else
-                    ball.dx = ball.dx / paddleBounce
-                    ball.dy = ball.dy / paddleBounce
                 end
-            else
+            elseif ball.x + ballSize <= 0 then
                 -- left misses ball
                 right.score = right.score + 1
                 resetBall()
             end
         elseif ball.x + ballSize > right.x then
             -- ball is crossing into right
-            if ball.y > right.y - ballSize and ball.y < right.y + paddleHeight then
+            if ball.x + ballSize < right.x + paddleWidth and ball.y > right.y - ballSize and ball.y < right.y + paddleHeight then
                 -- right hits ball
                 ball.x = right.x - ballSize
                 ball.dx = -ball.dx
                 if right.moving then
                     ball.dx = ball.dx * paddleBounce
                     ball.dy = ball.dy * paddleBounce
-                else
-                    ball.dx = ball.dx / paddleBounce
-                    ball.dy = ball.dy / paddleBounce
                 end
-            else
+            elseif ball.x >= love.graphics.getWidth() then
                 -- right misses ball
                 left.score = left.score + 1
                 resetBall()
@@ -178,4 +185,9 @@ function love.draw()
     
     -- Draw ball
     love.graphics.rectangle("fill", ball.x, ball.y, ballSize, ballSize)
+    
+    -- Draw Debug Info
+    if debugMode then
+       love.graphics.print("FPS: "..tostring(love.timer.getFPS()), love.graphics.getWidth() - 60, love.graphics.getHeight() - 20)
+    end
 end
