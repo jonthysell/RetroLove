10e9590ab221c3ace958866c52c85e6db6a366f3
Jon Thysell <thysell@gmail.com>
2017-11-02 11:39:49 -0700

    Breakout: Initial commit

---
+++ breakout/conf.lua
@@ -0,0 +1,9 @@
+-- breakout\conf.lua
+-- Copyright (c) 2017 Jon Thysell
+
+function love.conf(t)
+    t.window.title = "Breakout"
+    t.window.resizable = true
+    t.window.minwidth = 320
+    t.window.minheight = 240
+end
